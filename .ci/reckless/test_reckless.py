f317818246e73e70ff788b34d62bb3aa470d58fc
daywalker90 <daywalker990@gmx.de>
2026-06-30 17:02:51 +0200

    reckless only on daywalker runs

--- .ci/reckless/test_reckless.py
+++ .ci/reckless/test_reckless.py
@@ -10,83 +10,62 @@ from pyln.testing.fixtures import *  # noqa: F403
 LOGGER = logging.getLogger(__name__)
 
 
-def test_plugin_install_raw(node_factory, plugin_name):
-    LOGGER.info(f"Testing reckless raw with: {plugin_name}")
-
-    l1 = node_factory.get_node()
-
-    ln_dir = str(Path(l1.info["lightning-dir"]).parent)
-
-    subprocess.run(
-        [
-            "reckless",
-            "-v",
-            "--json",
-            "-l",
-            ln_dir,
-            "--network",
-            "regtest",
-            "source",
-            "remove",
-            "https://github.com/lightningd/plugins",
-        ],
-        input="Y\n",
-        text=True,
-        capture_output=True,
-    )
-
-    subprocess.check_call(
-        [
-            "reckless",
-            "-v",
-            "--json",
-            "-l",
-            ln_dir,
-            "--network",
-            "regtest",
-            "source",
-            "add",
-            f"{os.environ['GITHUB_WORKSPACE']}",
-        ]
-    )
-
-    r, w = os.pipe()
-
-    subprocess.check_call(
-        [
-            "reckless",
-            "-v",
-            "--json",
-            "-l",
-            ln_dir,
-            "--network",
-            "regtest",
-            "install",
-            plugin_name,
-        ],
-        stdin=r,
-    )
-
-    plugin_list = l1.rpc.call("plugin", ["list"])
-
-    for plugin in plugin_list["plugins"]:
-        if plugin_name in plugin["name"]:
-            return
-    pytest.fail(f"Plugin {plugin_name} not installed")
-
-
-# def test_plugin_install(node_factory, plugin_name):
-#     LOGGER.info(f"Testing reckless with: {plugin_name}")
+# def test_plugin_install_raw(node_factory, plugin_name):
+#     LOGGER.info(f"Testing reckless raw with: {plugin_name}")
 
 #     l1 = node_factory.get_node()
 
-#     l1.rpc.call(
-#         "reckless", ["source", "remove", "https://github.com/lightningd/plugins"]
+#     ln_dir = str(Path(l1.info["lightning-dir"]).parent)
+
+#     subprocess.run(
+#         [
+#             "reckless",
+#             "-v",
+#             "--json",
+#             "-l",
+#             ln_dir,
+#             "--network",
+#             "regtest",
+#             "source",
+#             "remove",
+#             "https://github.com/lightningd/plugins",
+#         ],
+#         input="Y\n",
+#         text=True,
+#         capture_output=True,
 #     )
 
-#     l1.rpc.call("reckless", ["source", "add", f"{os.environ['GITHUB_WORKSPACE']}"])
+#     subprocess.check_call(
+#         [
+#             "reckless",
+#             "-v",
+#             "--json",
+#             "-l",
+#             ln_dir,
+#             "--network",
+#             "regtest",
+#             "source",
+#             "add",
+#             f"{os.environ['GITHUB_WORKSPACE']}",
+#         ]
+#     )
 
-#     l1.rpc.call("reckless", ["install", plugin_name])
+#     r, w = os.pipe()
+
+#     subprocess.check_call(
+#         [
+#             "reckless",
+#             "-v",
+#             "--json",
+#             "-l",
+#             ln_dir,
+#             "--network",
+#             "regtest",
+#             "install",
+#             plugin_name,
+#         ],
+#         stdin=r,
+#     )
 
 #     plugin_list = l1.rpc.call("plugin", ["list"])
 
@@ -94,3 +73,24 @@ def test_plugin_install_raw(node_factory, plugin_name):
 #         if plugin_name in plugin["name"]:
 #             return
 #     pytest.fail(f"Plugin {plugin_name} not installed")
+
+
+def test_plugin_install(node_factory, plugin_name):
+    LOGGER.info(f"Testing reckless with: {plugin_name}")
+
+    l1 = node_factory.get_node()
+
+    l1.rpc.call(
+        "reckless", ["source", "remove", "https://github.com/lightningd/plugins"]
+    )
+
+    l1.rpc.call("reckless", ["source", "add", f"{os.environ['GITHUB_WORKSPACE']}"])
+
+    l1.rpc.call("reckless", ["install", plugin_name, "-v", "--developer"])
+
+    plugin_list = l1.rpc.call("plugin", ["list"])
+
+    for plugin in plugin_list["plugins"]:
+        if plugin_name in plugin["name"]:
+            return
+    pytest.fail(f"Plugin {plugin_name} not installed")
